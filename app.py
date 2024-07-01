3862255b4bc2f327280aef74bf9cbef8db863fdd
DevanshUpadhyay26 <Devanshupadhyay26@gmail.com>
2024-07-02 01:12:08 +0530

    minor changes in prompts

--- app.py
+++ app.py
@@ -51,8 +51,7 @@ submit4 = st.button("Keywords Missing based on JD")
 submit5 = st.button("Percentage match")
 
 input_prompt1 = """
- You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
-  Please share your professional evaluation on whether the candidate's profile aligns with the role. 
+ You are an experienced Technical Human Resource Manager.  
  Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
 """
 input_prompt2 = """
